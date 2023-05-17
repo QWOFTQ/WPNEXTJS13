c6b2d4cf79f2ccd96dcc8b528c2506cad964faa2
QWOFTQ <rkdcofls3890@naver.com>
2023-05-17 12:33:13 +0900

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <main className="container mx-auto">
+      <h1 className="mt-10 text-2xl font-bold">About page</h1>
+      <div>
+        <p>자기 소개</p>
+      </div>
+    </main>
+  )
+}
---
+++ app/project/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Project() {
+  return (
+    <main className="container mx-auto">
+      <h1 className="mt-10 text-2xl font-bold">Project</h1>
+      <div>
+        <p>프로젝트 소개</p>
+      </div>
+    </main>
+  )
+}
